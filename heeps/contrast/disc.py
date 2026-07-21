288705ae40162ad84f86f5eab071b61f14dba8e1
IainHammond <dr.iain.hammond@gmail.com>
2026-07-21 15:03:26 +0200

    new grid update. new rdi function.

--- heeps/contrast/disc.py
+++ heeps/contrast/disc.py
@@ -13,15 +13,13 @@ from typing import Union
 import os
 
 from vip_hci.fits import open_fits, open_header
-from vip_hci.preproc import frame_crop, cube_subsample, cube_crop_frames, frame_shift
+from vip_hci.preproc import frame_crop, cube_crop_frames, frame_shift
 from vip_hci.fm import cube_inject_fakedisk
-from vip_hci.psfsub import pca
 
-from heeps.contrast.background import background
 from heeps.util.psf_template import psf_template
 from heeps.util.paralang import paralang
 
-__all__ = ['create_disc_sequence', 'postproc_disc']
+__all__ = ['create_disc_sequence', 'prepare_rdi_sequence']
 __author__ = "Iain Hammond"
 
 def create_disc_sequence(
@@ -29,12 +27,9 @@ def create_disc_sequence(
         on_axis_psf : Union[np.ndarray, None] = None,
         off_axis_psf : Union[np.ndarray, None] = None,
         transmission : Union[np.ndarray, None] = None,
-        rdi : bool = True,
-        rdi_mag : Union[int, float, None] = None,
-        rdi_duration : Union[int, float, None] = None,
+        seeing_q : Union[int] = 2,
         source_xy : Union[tuple, list, None] = None,
         extinction : Union[int, float] = 0,
-        tag : Union[str, None] = None,
         starphot : Union[float, int] = 1e11,
         imlib : str = "opencv",
         **conf
@@ -58,20 +53,12 @@ def create_disc_sequence(
     transmission : Union[np.ndarray, None], optional
         Coronagraph transmission. If ``None``, the appropriate transmission is loaded based
         on the instrument mode in conf. Uses VIP conventions.
-    rdi : bool, default=True
-        Whether to generate an RDI reference cube from a segment of the on‑axis sequence.
-    rdi_mag : Union[int, float, None], optional
-        Magnitude to use for the RDI reference. If ``None``, the magnitude of the science target
-        is used.
-    rdi_duration : Union[int, float, None], optional
-        Desired duration (in seconds) of the RDI reference sequence. If ``None``, a default of
-        20% of the on‑axis sequence length is used.
+    seeing_q : int, default=2
+        Seeing quartile (1-3) to select the PSF. Uses a default seeing of Q2 if not specified.
     source_xy : Union[tuple, list, None], optional
         (x, y) pixel coordinates of the source for which extinction is applied.
     extinction : Union[int, float], default=0
         Extinction in magnitudes to apply to the source at ``source_xy``.
-    tag : Union[str, None], optional
-        Tag to prepend to output filenames from earlier HEEPS runs.
     starphot : float, default=1e11
         Photometric scaling factor for the star.
     imlib : str, default="opencv"
@@ -86,12 +73,19 @@ def create_disc_sequence(
         Sequence with the fake disc injected into the on‑axis PSF cube.
     pa : numpy.ndarray
         Array of parallactic angles used for the injection.
-    psf_RDI : numpy.ndarray, optional
-        Reference cube for RDI (if ``rdi`` is ``True``). Returned as the third element when
-        ``rdi`` is enabled; otherwise only ``psf_ON`` and ``pa`` are returned.
     """
-    tag = "" if tag is None else "%s_"%tag
-    loadname = os.path.join(conf['dir_output'], '%s%s_PSF_%s_%s.fits'%(tag,'%s', conf['band'], conf['mode']))
+    # if mag is not in the grid, round to the closest available magnitude
+    conf["mag"] = _check_mag(conf["mag"])
+
+    # new grid folder structure, assuming your dir_current points to where the folders are saved
+    grid_dir = os.path.join(
+        conf['dir_current'],
+        f"{conf['band']}_{conf['mode']}_s=Q{seeing_q}_mag={conf['mag']}_{conf['duration']}s_{int(conf['dit'] * 100)}ms"
+    )
+
+    # convention for the April 2026 grid files
+    loadname = os.path.join(grid_dir, '%s_PSF_bckg1_%s_%s.fits' % ('%s', conf['band'], conf['mode']))
+
     if off_axis_psf is None:
         psf_OFF = open_fits(loadname % 'offaxis', verbose=False)
         print(f"Using off-axis PSF from {loadname%'offaxis'}")
@@ -122,8 +116,8 @@ def create_disc_sequence(
         disc_model = disc_model[1:, 1:]
 
     # crop everything to a common size
-    # the HEEPS PSFs are usually 293px
     min_crop = min(psf_OFF.shape[-1], disc_model.shape[-1], conf["ndet"])
+    conf["ndet"] = min_crop
     if psf_OFF.shape[-1] > min_crop:
         psf_OFF = frame_crop(psf_OFF, min_crop, verbose=False)
     if disc_model.shape[-1] > min_crop:
@@ -134,6 +128,7 @@ def create_disc_sequence(
         source_x, source_y = source_xy
         extinction_factor = 10 ** (-0.4 * extinction)
         disc_model[source_y, source_x] *= extinction_factor
+        print("Applied extinction of %.2f mag to source at (x, y) = (%d, %d)" % (extinction, source_x, source_y), flush=True)
 
     # load coronagraph transmission depending on the mode and band
     if transmission is None:
@@ -149,9 +144,9 @@ def create_disc_sequence(
         else: # TODO
             raise NotImplementedError(f"Mode {conf['mode']} not implemented for disc injection.")
 
-    # determine how many frames are in the on-axis sequence (don't open the whole cube yet to save memory)
+    # determine how many frames are in the on-axis sequence
     if on_axis_psf is None:
-        nframes = open_header(loadname % 'onaxis')['NAXIS3']
+        nframes = open_header(loadname % 'onaxis')['NAXIS3']  # don't open the whole cube yet to save memory
     else:
         nframes = on_axis_psf.shape[0]
 
@@ -171,7 +166,7 @@ def create_disc_sequence(
         ker=1,
         imlib=imlib,
     )
-
+    # write_fits(conf["dir_current"] + "/prepared_model.fits", cube[0])
     # normalise by the stellar flux that we removed earlier
     cube /= star_val
 
@@ -190,137 +185,178 @@ def create_disc_sequence(
     psf_ON += cube
     del cube
 
-    # background addition
-    if conf["add_bckg"]:
-        psf_ON, psf_OFF = background(psf_ON, psf_OFF, verbose=True, **conf)
-
     _, _, ap_flux = psf_template(psf_OFF)
     psf_ON *= starphot / ap_flux
 
     # RDI handling (at the moment we only support taking a chunk out of the science sequence
-    if rdi:
-        if rdi_duration is None:
-            print("Warning: rdi_duration was not set. Using 20% of on-axis sequence.", flush=True)
-            rdi_duration = int(0.2 * nframes) * conf["dit"]
-        n_ref_frames = rdi_duration / conf["dit"]
-        start_idx = np.random.randint(0, psf_ON.shape[0] - int(n_ref_frames) + 1)  # random segment of the on-axis PSF cube to use as the RDI reference, to better match the background conditions
+    # if rdi:
+    #     if rdi_duration is None:
+    #         print("Warning: rdi_duration was not set. Using 20% of on-axis sequence.", flush=True)
+    #         rdi_duration = int(0.2 * nframes) * conf["dit"]
+    #     n_ref_frames = rdi_duration / conf["dit"]
+    #     start_idx = np.random.randint(0, psf_ON.shape[0] - int(n_ref_frames) + 1)  # random segment of the on-axis PSF cube to use as the RDI reference, to better match the background conditions
+    #
+    #     if on_axis_psf is None:  # reload the on-axis sequence
+    #         psf_RDI = open_fits(loadname % "onaxis", verbose=False)[start_idx:start_idx + int(n_ref_frames)]
+    #     else:
+    #         psf_RDI = on_axis_psf[start_idx:start_idx + int(n_ref_frames)]
+    #     del on_axis_psf
+    #
+    #     if psf_RDI.shape[-1] > min_crop:
+    #         psf_RDI = cube_crop_frames(psf_RDI, min_crop, verbose=False)
+    #
+    #     # remove the RDI segment from psf_ON to correctly represent lost integration time on the science target
+    #     psf_ON = np.concatenate([psf_ON[:start_idx], psf_ON[start_idx + int(n_ref_frames):]], axis=0)
+    #     pa = np.concatenate([pa[:start_idx], pa[start_idx + int(n_ref_frames):]], axis=0)
+    #
+    #     if off_axis_psf is None:
+    #         psf_RDI_OFF = open_fits(loadname % "offaxis", verbose=False)
+    #     else:
+    #         psf_RDI_OFF = off_axis_psf
+    #
+    #     if psf_RDI_OFF.shape[-1] > min_crop:
+    #         psf_RDI_OFF = frame_crop(psf_RDI_OFF, min_crop, verbose=False)
+    #
+    #     if rdi_mag is None:
+    #         print("Warning: rdi_mag was not set. Using mag of science target.", flush=True)
+    #     else:
+    #         conf["mag"] = rdi_mag
+    #
+    #     if conf["add_bckg"]:
+    #         psf_RDI, psf_RDI_OFF = background(psf_RDI, psf_RDI_OFF, verbose=True, **conf)
+    #
+    #     _, _, ap_flux = psf_template(psf_RDI_OFF)
+    #     psf_RDI *= starphot / ap_flux
+    #     del psf_RDI_OFF
+    #
+    #     return psf_ON, pa, psf_RDI
+    #
+    # else:
+    return psf_ON, pa
 
-        if on_axis_psf is None:  # reload the on-axis sequence
-            psf_RDI = open_fits(loadname % "onaxis", verbose=False)[start_idx:start_idx + int(n_ref_frames)]
-        else:
-            psf_RDI = on_axis_psf[start_idx:start_idx + int(n_ref_frames)]
-        del on_axis_psf
 
-        if psf_RDI.shape[-1] > min_crop:
-            psf_RDI = cube_crop_frames(psf_RDI, min_crop, verbose=False)
+def prepare_rdi_sequence(
+        on_axis_psf : Union[np.ndarray, None] = None,
+        off_axis_psf : Union[np.ndarray, None] = None,
+        rdi_mag : Union[int, float, None] = None,
+        rdi_duration : Union[int, float, None] = None,
+        seeing_q : Union[int] = 2,
+        starphot : Union[float, int] = 1e11,
+        **conf
+):
+        """
+        Prepare a sequence to be used as a reference for RDI (Reference Differential Imaging) from the PSF grid.
+        The function extracts a segment of the on-axis PSF cube to serve as the RDI reference depending on duration.
 
-        # remove the RDI segment from psf_ON to correctly represent lost integration time on the science target
-        psf_ON = np.concatenate([psf_ON[:start_idx], psf_ON[start_idx + int(n_ref_frames):]], axis=0)
-        pa = np.concatenate([pa[:start_idx], pa[start_idx + int(n_ref_frames):]], axis=0)
+        Parameters
+        ----------
+        on_axis_psf : Union[np.ndarray, None], optional
+            Pre‑loaded on‑axis PSF cube. If ``None``, the PSF is loaded based off conf and rdi_mag.
+            using the conf parameters.
+        off_axis_psf : Union[np.ndarray, None], optional
+            Pre‑loaded off‑axis PSF frame. If ``None``, the PSF is loaded based off conf and rdi_mag.
+        rdi_mag : Union[int, float, None], optional
+            Magnitude to use for the RDI reference. If ``None``, magnitude in conf is used.
+        rdi_duration : Union[int, float, None], optional
+            Desired duration (in seconds) of the RDI reference sequence. If ``None``, a default of
+            20% of the on‑axis sequence length is used.
+        seeing_q : int, default=2
+            Seeing quartile (1-3) to select the PSF. Uses a default seeing of Q2 if not specified.
+        starphot : float, default=1e11
+            Photometric scaling factor for the star.
+
+        Returns
+        -------
+        psf_RDI : numpy.ndarray
+            Reference cube for RDI.
+        """
+        if rdi_mag is None:
+            rdi_mag = conf["mag"]
+            print("Warning: rdi_mag was not set. Using mag in the conf dictionary.", flush=True)
+
+        rdi_mag = _check_mag(rdi_mag)
+
+        grid_dir = os.path.join(
+            conf['dir_current'],
+            f"{conf['band']}_{conf['mode']}_s=Q{seeing_q}_mag={rdi_mag}_{conf['duration']}s_{int(conf['dit'] * 100)}ms"
+        )
+
+        # convention for the April 2026 grid files, assuming dir_current points to the folder containing the PSF files
+        loadname = os.path.join(grid_dir, '%s_PSF_bckg1_%s_%s.fits' % ('%s', conf['band'], conf['mode']))
 
         if off_axis_psf is None:
-            psf_RDI_OFF = open_fits(loadname % "offaxis", verbose=False)
+            psf_OFF = open_fits(loadname % 'offaxis', verbose=False)
+            print(f"Using off-axis PSF from {loadname%'offaxis'}")
         else:
-            psf_RDI_OFF = off_axis_psf
+            psf_OFF = off_axis_psf
+        assert psf_OFF.ndim == 2, "off-axis PSF frame must be 2-dimensional"
 
-        if psf_RDI_OFF.shape[-1] > min_crop:
-            psf_RDI_OFF = frame_crop(psf_RDI_OFF, min_crop, verbose=False)
-
-        if rdi_mag is None:
-            print("Warning: rdi_mag was not set. Using mag of science target.", flush=True)
+        if on_axis_psf is None:
+            psf_ON = open_fits(loadname % 'onaxis', verbose=False)
+            print(f"Using on-axis PSF from {loadname % 'onaxis'}")
         else:
-            conf["mag"] = rdi_mag
+            psf_ON = on_axis_psf
+        assert psf_ON.ndim == 3, "on-axis PSF cube must be 3-dimensional"
 
-        if conf["add_bckg"]:
-            psf_RDI, psf_RDI_OFF = background(psf_RDI, psf_RDI_OFF, verbose=True, **conf)
+        if psf_OFF.shape[-1] > conf["ndet"]:
+            psf_OFF = frame_crop(psf_OFF, conf["ndet"], verbose=False)
 
-        _, _, ap_flux = psf_template(psf_RDI_OFF)
+        if psf_ON.shape[-1] > conf["ndet"]:
+            psf_ON = cube_crop_frames(psf_ON, conf["ndet"], verbose=False)
+
+        if rdi_duration is None:
+            rdi_duration = int(psf_ON.shape[0] * 0.2) * conf["dit"]
+            print("Warning: rdi_duration was not set. Using 20% of full sequence.", flush=True)
+
+        # convert into number of frames to extract from the on-axis PSF cube
+        n_ref_frames = rdi_duration / conf["dit"]
+
+        # check if the requested reference duration is longer than the available on-axis PSF cube
+        if n_ref_frames > psf_ON.shape[0]:
+            n_ref_frames = psf_ON.shape[0]
+            print(f"WARNING! Requested RDI duration ({rdi_duration}s) exceeded available on-axis PSF cube length "
+                  f"({psf_ON.shape[0] * conf['dit']}s). Setting them to be the same.", flush=True)
+
+        # random segment of the on-axis PSF cube to use as the RDI reference
+        start_idx = np.random.randint(0, psf_ON.shape[0] - int(n_ref_frames) + 1)
+        psf_RDI = psf_ON[start_idx:start_idx + int(n_ref_frames)]
+
+        _, _, ap_flux = psf_template(psf_OFF)
         psf_RDI *= starphot / ap_flux
-        del psf_RDI_OFF
+        del psf_OFF
 
-        return psf_ON, pa, psf_RDI
-
-    else:
-        return psf_ON, pa
+        return psf_RDI
 
 
-def postproc_disc(
-        cube : np.ndarray,
-        angle_list : np.ndarray,
-        cube_ref : Union[np.ndarray, None] = None,
-        subsample : int = 1,
-        ncomp: Union[tuple, list, int] = 1,
-        mask_center_px : int = 0,
-        imlib : str = "vip-fft",
-        source_xy : Union[tuple, None] = None,
-        delta_rot : Union[float, int, None] = 1,
-        fwhm : Union[float, int] = 5,
-        mask_rdi: np.ndarray = None,
-        ref_strategy: str = 'RDI',
-        nproc : int = 1
-):
+def _check_mag(mag : Union[float, int] ) -> float:
     """
-    Perform PCA-based post-processing on a disc‑injected data cube.
+    Check if the provided magnitude is in the Liege grid. If not, round to the closest available magnitude.
 
     Parameters
     ----------
-    cube : np.ndarray
-        3‑D array (n_frames, ny, nx) containing the science frames.
-    angle_list : np.ndarray
-        1‑D array of parallactic angles associated with each frame in ``cube``.
-    cube_ref : np.ndarray or None, optional
-        Reference cube for RDI (reference differential imaging). If ``None``, only ADI is performed.
-    subsample : int, default=1
-        Factor by which to down‑sample the cube (and reference) for speed. ``1`` means no subsampling.
-    ncomp : int, tuple, or list, default=1
-        Number of principal components to use. If an int, a single component is used. If a tuple ``(min, max)`` or a
-        list, the function will compute results for each component in the range.
-    mask_center_px : int, default=0
-        Radius (in pixels) of a circular mask applied to the centre of each frame.
-    imlib : str, default="vip-fft"
-        Image library used by VIP for FFT‑based operations.
-    source_xy : tuple or None, optional
-        (x, y) pixel coordinates of a known source; used for frame rejection in the PCA library.
-    delta_rot : float or int or None, default=1
-        Minimum rotation (in FWHM) between frames.
-    fwhm : float or int, default=5
-        Full‑width at half‑maximum of the PSF, used for delta_rot.
-    mask_rdi : np.ndarray, optional
-        See description in VIP.
-    ref_strategy : str, default='RDI'
-        Strategy for reference handling (RDI or ARDI)
-    nproc : int, default=1
-        Number of processors to use for parallel computation.
+    mag : float, int
+        The magnitude to check.
 
     Returns
     -------
-    np.ndarray
-        Array of shape ``(n_ncomp, ny, nx)`` where ``n_ncomp`` is the number of principal components evaluated.
-        Each slice ``res[i]`` contains the PCA‑processed image for the corresponding number of components.
-
-    Notes
-    -----
-    The function currently supports the subset of ``vip_hci.psfsub.pca`` parameters used in the HEEPS disc injection
-     workflow. Additional ``pca`` arguments can be added in the future.
+    new_mag : float
+        The closest available magnitude in the Liege grid.
     """
-    # subsample the cube and the reference if requested, for efficiency purposes
-    if subsample > 1:
-        cube, angle_list = cube_subsample(array=cube, n=subsample, parallactic=angle_list)
-        if cube_ref is not None:
-            cube_ref = cube_subsample(array=cube_ref, n=subsample)
+    mag_og = float(mag)
 
-    # loop over principal components
-    if isinstance(ncomp, int):
-        ncomp = [ncomp]
-    elif isinstance(ncomp, tuple):
-        ncomp = np.arange(ncomp[0], ncomp[-1]+1)
+    allowed = np.arange(-1.5, 9.0 + 0.5, step=0.5)
 
-    res = np.zeros([len(ncomp), cube.shape[-2], cube.shape[-1]])
+    if mag not in allowed:
+        mag = round(mag / 0.5) * 0.5
 
-    print("Running PCA post-processing", flush=True)
-    for i, npc in enumerate(ncomp):
-        res[i] = pca(cube, angle_list=angle_list, cube_ref=cube_ref, ncomp=npc,
-                  mask_center_px=mask_center_px, imlib=imlib, nproc=nproc,
-                  source_xy=source_xy, delta_rot=delta_rot, fwhm=fwhm, mask_rdi=mask_rdi,
-                  ref_strategy=ref_strategy)
-    return res
+    # clamp to valid range
+    if mag < allowed[0]:
+        mag = allowed[0]
+        print(f"Warning: mag={mag_og} is below minimum. Using mag={mag}")
+    elif mag > allowed[-1]:
+        mag  = allowed[-1]
+        print(f"Warning: mag={mag_og} is above maximum. Using mag={mag}")
+    elif mag != mag_og:
+        print(f"Warning: mag={mag_og} rounded to nearest grid value mag={mag}")
+
+    return mag
